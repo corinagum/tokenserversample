769fb70c6cc8a7016d2ef81298af9585bca9a587
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-21 17:40:08 -0700

    Create token server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv/config');
 const restify = require('restify');
 const { join } = require('path');
+const fetch = require('node-fetch');
 const server = restify.createServer();
 
 server.get(
@@ -10,5 +12,28 @@ server.get(
 );
 
 server.listen(process.env.PORT || 80, () => {
-  console.log('tokenserversample server running');
-})
+  console.log('Token Server Sample server running');
+});
+
+server.get(
+  '/api/token',
+  async (req, res) => {
+    const cres = await fetch(
+      'https://directline.botframework.com/v3/directline/tokens/generate',
+      {
+        headers: {
+          accept: 'text/javascript',
+          authorization: `Bearer ${ process.env.DIRECT_LINE_SECRET }`
+        },
+        method: 'POST'
+      }
+    )
+    if (cres.ok) {
+      const json = await cres.json();
+      res.json(json);
+    } else {
+      res.send(500);
+      console.log(await cres.text());
+    }
+  }
+);
