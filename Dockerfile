e6a6706c74bd10a964cebc4d8b682346adf06f98
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-21 18:37:10 -0700

    Set up SSH

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,30 @@
-FROM node:alpine
-EXPOSE 80
+FROM node:12
+EXPOSE 80 2222
+
+ADD scripts/init.sh /usr/local/bin/
+
+# Setup OpenSSH for debugging thru Azure Web App
+# https://docs.microsoft.com/en-us/azure/app-service/containers/app-service-linux-ssh-support#ssh-support-with-custom-docker-images
+# https://docs.microsoft.com/en-us/azure/app-service/containers/tutorial-custom-docker-image
+ENV SSH_PASSWD "root:Docker!"
+ENV SSH_PORT 2222
+RUN \
+  apt-get update \
+  && apt-get install -y --no-install-recommends dialog \
+  && apt-get update \
+  && apt-get install -y --no-install-recommends openssh-server \
+  && echo "$SSH_PASSWD" | chpasswd \
+  && chmod u+x /usr/local/bin/init.sh
+
+ADD scripts/sshd_config /etc/ssh/
 
 WORKDIR /var/web/
-ENTRYPOINT node server.js
+
+ENTRYPOINT init.sh
 
 ADD . /var/web/
 
 RUN npm ci
 
-#name: tss
+# docker tag ___ tss
 #run, then remove: docker run -p 80:80 --rm -it tss
--- server.js
+++ server.js
@@ -37,3 +37,57 @@ server.get(
     }
   }
 );
+
+const trustedOrigin = (url) => {
+  return (
+    /^https?:\/\/localhost([\/:]|$)/.test(url)
+  );
+}
+
+const generateDirectLineToken = async () => {
+  const { DIRECT_LINE_SECRET } = process.env;
+
+  console.log(`Generating Direct Line token using secret "${ DIRECT_LINE_SECRET.substr(0, 3) }...${ DIRECT_LINE_SECRET.substr(-3) }"`);
+
+  let cres;
+
+  cres = await fetch(`https://directline.botframework.com/v3/directline/tokens/generate`, {
+    headers: {
+      authorization: `Bearer ${ DIRECT_LINE_SECRET }`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  });
+
+  if (cres.status === 200) {
+    const json = await cres.json();
+
+    if ('error' in json) {
+      throw new Error(`Direct Line service responded ${ JSON.stringify(json.error) } while generating new token`);
+    } else {
+      return json;
+    }
+  } else {
+    throw new Error(`Direct Line service returned ${ cres.status } while generating new token`);
+  }
+}
+
+
+server.post('/directline/token', async (req, res) => {
+  const origin = req.header('origin');
+
+  if(!trustedOrigin(origin)) {
+    return res.send(403, 'Origin not trusted');
+  }
+
+  try {
+    res.send(await generateDirectLineToken(), { 'Access-Control-Allow-Origin': '*'});
+  } catch (error) {
+    res.send(500, error.message, { 'Access-Control-Allow-Origin': '*'});
+  }
+
+  const { DIRECT_LINE_SECRET } = process.env;
+
+
+  console.log(`Requesting Direct Line token for ${ origin } using secret "${ DIRECT_LINE_SECRET.substr(0, 3) }...${ DIRECT_LINE_SECRET.substr(-3) }"`);
+});
